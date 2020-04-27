bc63b24b2709f024d4218a51503d922a6a6e74b5
andriar.rizkiansah <andriar.rizkiansah@mavila.co.id>
2020-04-27 18:21:40 +0700

    fixed image size

--- ga.js
+++ ga.js
@@ -10,7 +10,7 @@
 	a.async = 1;
 	a.src = g;
 	m.parentNode.insertBefore(a, m);
-})(window, document, 'script', '//www.google-analytics.com/analytics.js', 'ga');
+})(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
 
 ga('create', 'UA-48323419-1', 'auto');
 ga('send', 'pageview');
